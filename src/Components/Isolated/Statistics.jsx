d8261ee3fc99a281872aabe11eea14835caa2448
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-04-03 04:10:18 +0600

    statistic added

---
+++ src/Components/Isolated/Statistics.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Statistics() {
+  const statistics = [
+    { _id: 1, qty: "১৩+", activity: "সংবাদ উপস্থাপনের অভিজ্ঞতা" },
+    { _id: 2, qty: "১০০+", activity: "গণমাধ্যমকর্মী প্রস্তুত করা" },
+    { _id: 3, qty: "২১০", activity: "প্রজেক্ট কমপ্লিট" },
+    { _id: 4, qty: "২৭", activity: "অ্যাওয়ার্ড ইউনার" },
+    { _id: 5, qty: "১১০", activity: "সার্টিফিকেট কোর্স সম্পন্ন" },
+    { _id: 6, qty: "২ মি.", activity: "তরুণকে ইন্সপায়ার করা" },
+    { _id: 7, qty: "৫ মি.", activity: "ফ্যান ফলোয়ার্স" },
+    { _id: 8, qty: "১১০", activity: "বায়ো ইন্টার্ভিউ রাইটিং" },
+  ];
+  return (
+    <div className="my-14 lg:mx-24">
+      <div className="grid lg:grid-cols-8 grid-cols-1 gap-2">
+        {statistics.map((statistic) => (
+          <div
+            key={statistic._id}
+            className="px-5 py-10 border-2 border-[#860E3F] flex flex-col items-center text-[#fff]"
+          >
+            <h3 className="text-xl">{statistic.qty}</h3>
+            <h3 className="text-center text-xl">{statistic.activity}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Statistics;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Components/Isolated/Banner";
+import Statistics from "../Components/Isolated/Statistics";
 
 function Home() {
   return (
     <div>
       <Banner />
+      <Statistics />
     </div>
   );
 }
