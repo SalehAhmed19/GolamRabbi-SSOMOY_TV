3c5882335bad6e28560f0d282ae748685fe86f87
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-04-03 02:31:03 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Shared/Navbar";
+import Home from "./Pages/Home";
 
 function App() {
-  return <div className="App">Home</div>;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#F3B004",
+      },
+      secondary: {
+        main: "#F80264",
+      },
+      accent: {
+        main: "#B8084F",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/Components/Shared/Navbar.jsx
@@ -0,0 +1,189 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import logo from "../../assets/logo/logo.png";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const drawerWidth = 240;
+const upperNavItems = ["লগইন", "সাবস্ক্রাইব"];
+const lowerNavItems = ["চাকরি", "ইভেন্ট", "কোর্স", "ব্লগ", "ইনভাইট"];
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#F3B004",
+    },
+    secondary: {
+      main: "#F80264",
+    },
+    accent: {
+      main: "#B8084F",
+    },
+  },
+  spacing: 0, // sets the default spacing to 4px
+  components: {
+    AppBar: {
+      styleOverrides: {
+        root: {
+          padding: "0px", // sets the padding for AppBar
+        },
+      },
+    },
+  },
+});
+
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Box variant="h6" sx={{ my: 2 }}>
+        <img className="w-20 p-5 mx-auto block" src={logo} alt="" />
+      </Box>
+      <Divider />
+      <List>
+        {upperNavItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CssBaseline />
+        <AppBar component="nav" sx={{ backgroundColor: "transparent" }}>
+          <Toolbar>
+            {/* Upper Menu */}
+            <div className="w-full">
+              <div className="w-full flex p-5 bg-[#000]">
+                <IconButton
+                  color="primary"
+                  aria-label="open drawer"
+                  edge="start"
+                  onClick={handleDrawerToggle}
+                  sx={{ display: { sm: "none" }, padding: "0px" }}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Box
+                  component="div"
+                  sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+                >
+                  <div className="flex items-center lg:px-14">
+                    <IconButton
+                      color="primary"
+                      aria-label="open drawer"
+                      edge="start"
+                      onClick={handleDrawerToggle}
+                      // sx={{ mr: 2 }}
+                      sx={{ marginRight: "20px" }}
+                    >
+                      <MenuIcon />
+                    </IconButton>
+                    <input
+                      className="bg-transparent py-2 px-5 rounded-full border-2 text-center text-[#fff]"
+                      type="text"
+                      placeholder="সার্চ বার"
+                    />
+                  </div>
+                </Box>
+                <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                  {upperNavItems.map((item) => (
+                    <Button
+                      key={item}
+                      sx={{ color: "#FBAF18", fontSize: "18px" }}
+                    >
+                      {item}
+                    </Button>
+                  ))}
+                </Box>
+              </div>
+              {/* Lower Menu */}
+              <div className="w-full flex p-5 bg-[#0000004b] lg:px-14">
+                <Box
+                  component="div"
+                  sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+                >
+                  <div className="flex items-center">
+                    <img className="w-10" src={logo} alt="" />{" "}
+                    <p className="text-[#fff] font-bold text-xl ml-2">
+                      Golam Rabbi
+                    </p>
+                  </div>
+                </Box>
+                <Box
+                  sx={{
+                    display: { xs: "none", sm: "block" },
+                  }}
+                >
+                  {lowerNavItems.map((item) => (
+                    <Button key={item} sx={{ color: "#fff", padding: "0px" }}>
+                      {item}
+                    </Button>
+                  ))}
+                </Box>
+              </div>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+Navbar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default Navbar;
