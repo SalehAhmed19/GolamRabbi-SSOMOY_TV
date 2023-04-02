beab0b3e8bc741899ca95c0cea10f49afccc638e
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-04-03 05:41:14 +0600

    job circular added

---
+++ src/Components/Isolated/MediaJobCircular.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import "../../Styles/MediaJobCircular.css";
+import bg from "../../assets/images/job-bg.jpg";
+
+function MediaJobCircular() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+    breakpoints: {
+      "(min-width: 400px)": {
+        slides: { perView: 2, spacing: 5 },
+      },
+      "(min-width: 1000px)": {
+        slides: { perView: 4, spacing: 40 },
+      },
+    },
+    slides: { perView: 1 },
+  });
+  return (
+    <div
+      style={{
+        background: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="pb-10"
+    >
+      <h2 className="text-primary text-center py-10 text-4xl font-bold">
+        মিডিয়া জব সার্কুলার
+      </h2>
+      <div className="lg:mx-24">
+        <div className="navigation-wrapper">
+          <div ref={sliderRef} className="keen-slider">
+            <div className="keen-slider__slide h-48 w-20 border-[#59D8DE] border-2 bg-[#212429] rounded-lg"></div>
+            <div className="keen-slider__slide h-48 w-20 border-[#59D8DE] border-2 bg-[#212429] rounded-lg"></div>
+            <div className="keen-slider__slide h-48 w-20 border-[#59D8DE] border-2 bg-[#212429] rounded-lg"></div>
+            <div className="keen-slider__slide h-48 w-20 border-[#59D8DE] border-2 bg-[#212429] rounded-lg"></div>
+            <div className="keen-slider__slide h-48 w-20 border-[#59D8DE] border-2 bg-[#212429] rounded-lg"></div>
+            <div className="keen-slider__slide h-48 w-20 border-[#59D8DE] border-2 bg-[#212429] rounded-lg"></div>
+          </div>
+        </div>
+        {loaded && instanceRef.current && (
+          <div className="dots mt-10">
+            {[
+              ...Array(
+                instanceRef.current.track.details.slides.length - 3
+              ).keys(),
+            ].map((idx) => {
+              return (
+                <button
+                  key={idx}
+                  onClick={() => {
+                    instanceRef.current?.moveToIdx(idx);
+                  }}
+                  className={"dot" + (currentSlide === idx ? " active" : "")}
+                ></button>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default MediaJobCircular;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Advertisement from "../Components/Isolated/Advertisement";
 import Banner from "../Components/Isolated/Banner";
 import FanFun from "../Components/Isolated/FanFun";
 import Features from "../Components/Isolated/Features";
+import MediaJobCircular from "../Components/Isolated/MediaJobCircular";
 import Statistics from "../Components/Isolated/Statistics";
 
 function Home() {
@@ -13,6 +14,7 @@ function Home() {
       <Features />
       <FanFun />
       <Advertisement />
+      <MediaJobCircular />
     </div>
   );
 }
