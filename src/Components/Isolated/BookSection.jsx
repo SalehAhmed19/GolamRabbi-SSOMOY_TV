0f0f1c8ffadf402a7ee9f4ae8b42d3b28d70e823
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-04-11 16:41:09 +0600

    some issue fixed

--- src/Components/Isolated/BookSection.jsx
+++ src/Components/Isolated/BookSection.jsx
@@ -14,11 +14,13 @@ function BookSection() {
     >
       <div>
         <div className="mb-5 text-center">
-          <h3 className="text-primary font-bold text-4xl">বই মেলায় আসছে</h3>
+          <h3 className="text-primary font-bold text-4xl mb-2">
+            বই মেলায় আসছে
+          </h3>
           <p className="text-basic text-xl font-bold">এপ্রিল ১৪</p>
         </div>
-        <img className="w-72 mx-auto shadow-xl" src={book} alt="" />
-        <button className="block mx-auto my-10 py-2 border-2 border-[#AF1453] hover:bg-[#AF1453] w-[290px] text-center rounded-full text-[#fff] font-bold text-xl">
+        <img className="w-72 mx-auto shadow-2xl" src={book} alt="" />
+        <button className="block shadow-lg mx-auto my-10 py-2 border-2 border-[#AF1453] hover:bg-[#AF1453] w-[290px] text-center rounded-full text-[#fff] font-bold text-xl">
           কিনতে ক্লিক করো
         </button>
       </div>
