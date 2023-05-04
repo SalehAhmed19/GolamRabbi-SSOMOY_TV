3783807aa8d2a2a49330604f4ed21f297b09f092
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-05-04 19:17:25 +0600

    book section added

--- src/Components/Isolated/BookSection.jsx
+++ src/Components/Isolated/BookSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 // import bg from "../../assets/images/book-bg.png";
-import book from "../../assets/images/book1.jpg";
+import book from "../../assets/images/book.png";
 
 // Import Swiper styles
 import "swiper/css";
@@ -14,116 +14,136 @@ import { EffectCoverflow, Pagination } from "swiper";
 function BookSection() {
   return (
     <>
-
-
-    <div
-      // style={{
-      //   background: `url(${bg})`,
-      //   backgroundSize: "cover",
-      //   backgroundRepeat: "no-repeat",
-      // }}
-      className="py-20"
-    >
-      <div>
-        <div className="mb-5 text-center">
-          <h3 className="text-primary font-bold text-4xl mb-2">
-            বই মেলায় আসছে
-          </h3>
-          <p className="text-basic text-xl font-bold">এপ্রিল ১৪</p>
+      <div
+        // style={{
+        //   background: `url(${bg})`,
+        //   backgroundSize: "cover",
+        //   backgroundRepeat: "no-repeat",
+        // }}
+        className="py-20"
+      >
+        <div>
+          <div className="mb-5 text-center">
+            <h3 className="text-primary font-bold text-4xl mb-2">
+              বই মেলায় আসছে
+            </h3>
+            <p className="text-basic text-xl font-bold">এপ্রিল ১৪</p>
+          </div>
+          {/* <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 4,
+              loop: true,
+              slideShadows: true,
+            }}
+            // pagination={true}
+            modules={[EffectCoverflow, Pagination, Autoplay]}
+            className="myswiper "
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+          >
+            <SwiperSlide>
+              <img
+                className="md:w-72 lg:w-96 mx-auto shadow-2xl"
+                src={book}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="md:w-72 lg:w-96 mx-auto shadow-2xl"
+                src={book}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="md:w-72 lg:w-96 mx-auto shadow-2xl"
+                src={book}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="md:w-72 lg:w-96 mx-auto shadow-2xl"
+                src={book}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="md:w-72 lg:w-96 mx-auto shadow-2xl"
+                src={book}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="md:w-72 lg:w-96 mx-auto shadow-2xl"
+                src={book}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="md:w-72 lg:w-96 mx-auto shadow-2xl"
+                src={book}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="md:w-72 lg:w-96 mx-auto shadow-2xl"
+                src={book}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="md:w-72 lg:w-96 mx-auto shadow-2xl"
+                src={book}
+                alt=""
+              />
+            </SwiperSlide>
+          </Swiper> */}
+          <div className="grid grid-cols-1 lg:grid-cols-2 text-primary bg-[#111118] w-3/4 mx-auto rounded-md">
+            <div className="pl-10 pt-10 flex flex-col justify-center">
+              <h2 className="font-bold text-3xl mb-3">কি যে করি</h2>
+              <p>
+                জীবের মধ্যে সবচেয়ে সম্পূর্ণতা মানুষের। কিন্তু সবচেয়ে অসম্পূর্ণ
+                হয়ে সে জন্মগ্রহণ করে। বাঘ ভালুক তার জীবনযাত্রার পনেরো- আনা
+                মূলধন নিয়ে আসে প্রকৃতির মালখানা থেকে। জীবরঙ্গভূমিতে মানুষ এসে
+                দেখা দেয় দুই শূন্য হাতে মুঠো বেঁধে। মানুষ আসবার পূর্বেই
+                জীবসৃষ্টিযজ্ঞে প্রকৃতির ভূরিব্যয়ের পালা শেষ হয়ে এসেছে। বিপুল
+                মাংস, কঠিন বর্ম, প্রকাণ্ড লেজ নিয়ে জলে স্থলে পৃথুল দেহের যে
+                অমিতাচার প্রবল হয়ে উঠেছিল তাতে ধরিত্রীকে দিলে ক্লান্ত করে।
+                প্রমাণ হল আতিশয্যের পরাভব অনিবার্য। পরীক্ষায় এটাও স্থির হয়ে
+                গেল যে, প্রশ্রয়ের পরিমাণ যত বেশি হয় দুর্বলতার বোঝাও তত দুর্বহ
+                হয়ে ওঠে। নূতন পর্বে প্রকৃতি যথাসম্ভব মানুষের বরাদ্দ কম করে
+                দিয়ে নিজে রইল নেপথ্যে। মানুষকে দেখতে হল খুব ছোটো, কিন্তু সেটা
+                একটা কৌশল মাত্র। এবারকার জীবযাত্রার পালায় বিপুলতাকে করা হল
+                বহুলতায় পরিণত। মহাকায় জন্তু ছিল প্রকাণ্ড একলা, মানুষ হল
+                দূরপ্রসারিত অনেক।
+              </p>
+              <button className="block shadow-lg mx-auto my-10 py-2 border-2 border-[#AF1453] hover:bg-[#AF1453] w-[290px] text-center rounded-full text-[#fff] font-bold text-xl">
+                কিনতে ক্লিক করো
+              </button>
+            </div>
+            <div className="p-10 flex justify-end">
+              <img className="w-72 drop-shadow-xl" src={book} alt="" />
+            </div>
+          </div>
         </div>
-        <Swiper
-          effect={"coverflow"}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={"auto"}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 4,
-            loop:true,
-            slideShadows: true,
-          }}
-          // pagination={true}
-          modules={[EffectCoverflow, Pagination,Autoplay]}
-          className="myswiper "
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-        >
-          <SwiperSlide>
-            <img
-              className="md:w-72 lg:w-96 mx-auto shadow-2xl"
-              src={book}
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="md:w-72 lg:w-96 mx-auto shadow-2xl"
-              src={book}
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="md:w-72 lg:w-96 mx-auto shadow-2xl"
-              src={book}
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="md:w-72 lg:w-96 mx-auto shadow-2xl"
-              src={book}
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="md:w-72 lg:w-96 mx-auto shadow-2xl"
-              src={book}
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="md:w-72 lg:w-96 mx-auto shadow-2xl"
-              src={book}
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="md:w-72 lg:w-96 mx-auto shadow-2xl"
-              src={book}
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="md:w-72 lg:w-96 mx-auto shadow-2xl"
-              src={book}
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="md:w-72 lg:w-96 mx-auto shadow-2xl"
-              src={book}
-              alt=""
-            />
-          </SwiperSlide>
-        </Swiper>
-        <button className="block shadow-lg mx-auto my-10 py-2 border-2 border-[#AF1453] hover:bg-[#AF1453] w-[290px] text-center rounded-full text-[#fff] font-bold text-xl">
-          কিনতে ক্লিক করো
-        </button>
       </div>
-     
-    </div>
     </>
-    
   );
 }
 
