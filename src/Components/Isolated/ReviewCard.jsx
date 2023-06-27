7da07c4d63641ffbd37ae5208c2d941223ec5fc9
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-06-27 19:34:28 +0600

    some changes on testimoinals

--- src/Components/Isolated/ReviewCard.jsx
+++ src/Components/Isolated/ReviewCard.jsx
@@ -3,25 +3,27 @@ import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
 const ReviewCard = ({ d }) => {
   return (
-    <div className="p-5 lg:p-10 shadow-lg flex flex-col lg:flex-row lg:w-4/5 mx-auto bg-[#111118] rounded-md">
+    <div className="py-10 px-5 shadow-lg flex flex-col items-center lg:flex-row  mx-auto bg-[#111118] rounded-md">
       <div>
-        <div className="bg-primary h-3 w-24 mx-auto rounded-t-md"></div>
-        <div className="lg:w-40 bg-[#fff] inline-block p-4 shadow-lg">
+        <div className="bg-primary h-3 w-14 mx-auto rounded-t-md"></div>
+        <div className="lg:w-20 bg-[#fff] inline-block p-2 shadow-lg">
           <img
-            className="lg:w-36 shadow-lg rounded-md mx-auto"
+            className="lg:w-20 shadow-lg rounded-md mx-auto"
             src={d.img}
             alt=""
           />
-          <h2 className="text-center font-bold my-2 text-[#333]">{d.name}</h2>
+          <h2 className="text-center font-bold my-2 text-[#333] text-sm">
+            {d.name}
+          </h2>
         </div>
-        <div className="bg-primary h-3 w-24 mx-auto rounded-b-md"></div>
+        <div className="bg-primary h-3 w-14 mx-auto rounded-b-md"></div>
       </div>
       <div>
         {/* <div className="bg-[#fff] h-2 w-40 mt-5"></div> */}
         {/* <h2 className="text-primary m-3 font-bold text-xl">অডিয়েন্স রিভিউ</h2> */}
         <div className="lg:mx-5">
           <FaQuoteLeft className="text-[#363636] text-3xl" />
-          <div className="lg:p-5 mt-5 lg:mt-0">
+          <div className=" mt-5 lg:mt-0">
             <p className="text-primary">{d.review}</p>
           </div>
           {/* <FaQuoteRight className="text-[#363636] text-3xl ml-auto" /> */}
