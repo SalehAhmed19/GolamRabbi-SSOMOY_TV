a142076ed9b1ddfc13d93cdd56ca33f7392abe1a
MohammadFaysal731 <www.faysal731@gmail.com>
2023-10-04 16:57:45 +0600

    add modal on gallery page

--- src/Components/Isolated/Modals/GalleryModal.jsx
+++ src/Components/Isolated/Modals/GalleryModal.jsx
@@ -2,8 +2,8 @@ import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import React from "react";
 import { MdOutlineClose } from "react-icons/md";
-
-const GalleryModal = ({ image, openModal, handleModalClose }) => {
+const GalleryModal = ({ galleryImage, openModal, setOpenModal }) => {
+  const handleModalClose = () => setOpenModal(false)
   const style = {
     position: "absolute",
     top: "50%",
@@ -14,45 +14,36 @@ const GalleryModal = ({ image, openModal, handleModalClose }) => {
     boxShadow: 24,
   };
   return (
-    <>
-      <Modal
-        open={openModal}
-        onClose={handleModalClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+    <Modal
+      open={openModal}
+      onClose={handleModalClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        sx={style}
+        className="p-5 2xs:w-[85%] xs:w-[90%] md:w-full md:h-full"
       >
-        <Box
-          sx={style}
-          className="p-5 2xs:w-[85%] xs:w-[90%] md:w-full md:h-full"
+        <div
+          className="h-[50vh] md:h-[90%] overflow-auto"
+          style={{ fontFamily: "Hind Siliguri" }}
         >
-          <div
-            className="h-[50vh] md:h-[90%] overflow-auto bg-white"
-            style={{ fontFamily: "Hind Siliguri" }}
-          >
-            <div className="font-bold text-secondary flex justify-between items-center">
-              {/* <h1 className="font-bold 2xs:text-[12px] xs:text-[20px] sm:text-[22px] md:text-[24px] lg:text-[26px]  my-5">
-                descriptionText?.webinar?.title
-              </h1> */}
-              <MdOutlineClose
-                onClick={handleModalClose}
-                className="2xs:text-[12px] xs:text-[20px] sm:text-[22px] md:text-[24px] lg:text-[26px]  cursor-pointer"
-              />
-            </div>
-            {/* <p className="text-justify 2xs:text-[12px]">
-              descriptionText?.webinar?.description ....
-            </p> */}
-
-            <img src={image.image} alt="" />
+          <div className="">
+            <MdOutlineClose
+              onClick={handleModalClose}
+              className="mb-2 text-secondary float-right 2xs:text-[14px] xs:text-[20px] sm:text-[22px] md:text-[24px] lg:text-[26px]  cursor-pointer"
+            />
+            <img src={galleryImage.image} alt="" />
+            <button
+              onClick={handleModalClose}
+              className="block ml-auto  px-10 py-1 rounded 2xs:my-4 xs:my-6 sm:my-8 md:my-10 bg-secondary text-white my-3 text-center 2xs:text-[12px]  sm:text-[14px] md:text-[16px] lg:text-[18px]"
+            >
+              বন্ধ করুন
+            </button>
           </div>
-          <button
-            onClick={handleModalClose}
-            className="block ml-auto  px-10 py-1 rounded 2xs:my-4 xs:my-6 sm:my-8 md:my-10 bg-secondary text-white my-3 text-center 2xs:text-[12px]  sm:text-[14px] md:text-[16px] lg:text-[18px]"
-          >
-            বন্ধ করুন
-          </button>
-        </Box>
-      </Modal>
-    </>
+        </div>
+      </Box>
+    </Modal>
   );
 };
 
--- src/Pages/ImageGalleryPage.jsx
+++ src/Pages/ImageGalleryPage.jsx
@@ -3,6 +3,9 @@ import { Fade } from "react-reveal";
 import "swiper/css";
 import "swiper/css/effect-cards";
 import GalleryCard from "../Components/Isolated/Cards/GalleryCard";
+import facebook from "../assets/SVGs/socilaIcons/facebook (1).png";
+import linkedin from "../assets/SVGs/socilaIcons/linkedin.png";
+import whatsapp from "../assets/SVGs/socilaIcons/whatsapp.png";
 import img1 from "../assets/images/gr1.jpg";
 import img2 from "../assets/images/gr2.jpg";
 import img3 from "../assets/images/gr3.jpg";
@@ -10,18 +13,102 @@ import img4 from "../assets/images/gr4.jpg";
 import img5 from "../assets/images/gr5.jpg";
 const ImageGalleryPage = () => {
   const galleryImages = [
-    { id: 1, image: img1, title: "Golam Rabby 1" },
-    { id: 2, image: img2, title: "Golam Rabby 2" },
-    { id: 3, image: img3, title: "Golam Rabby 3" },
-    { id: 4, image: img4, title: "Golam Rabby 4" },
-    { id: 5, image: img5, title: "Golam Rabby 5" },
-    { id: 6, image: img1, title: "Golam Rabby 6" },
-    { id: 7, image: img2, title: "Golam Rabby 7" },
-    { id: 8, image: img3, title: "Golam Rabby 8" },
-    { id: 9, image: img4, title: "Golam Rabby 9" },
-    { id: 10, image: img5, title: "Golam Rabby 10" },
-    { id: 11, image: img1, title: "Golam Rabby 11" },
-    { id: 12, image: img2, title: "Golam Rabby 12" },
+    {
+      id: 1,
+      image: img1,
+      title: "Golam Rabby 1",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 2,
+      image: img2,
+      title: "Golam Rabby 2",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 3,
+      image: img3,
+      title: "Golam Rabby 3",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 4,
+      image: img4,
+      title: "Golam Rabby 4",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 5,
+      image: img5,
+      title: "Golam Rabby 5",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 6,
+      image: img1,
+      title: "Golam Rabby 6",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 7,
+      image: img2,
+      title: "Golam Rabby 7",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 8,
+      image: img3,
+      title: "Golam Rabby 8",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 9,
+      image: img4,
+      title: "Golam Rabby 9",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 10,
+      image: img5,
+      title: "Golam Rabby 10",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 11,
+      image: img1,
+      title: "Golam Rabby 11",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
+    {
+      id: 12,
+      image: img2,
+      title: "Golam Rabby 12",
+      faceBookIcon: facebook,
+      linkedinIcon: linkedin,
+      whatsappIcon: whatsapp,
+    },
   ];
   return (
     <div className="lg:px-24 py-20 p-5 pb-36 text-[#fff] cursor-pointer select-none">
@@ -31,8 +118,8 @@ const ImageGalleryPage = () => {
         </h2>
       </Fade>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 gap-5">
-        {galleryImages.map((image, index) => (
-         <GalleryCard image={image} key={index}/>
+        {galleryImages.map((galleryImage, index) => (
+          <GalleryCard galleryImage={galleryImage} key={index} />
         ))}
       </div>
     </div>
